9d889066d69c92ab4d698ed001e4a795d6d2b511
Gnom204 <gnomek100500@gmail.com>
2025-09-08 19:39:56 +0300

    В С Ё

---
+++ ConsoleApp1/second.cs
@@ -0,0 +1,10 @@
+﻿string a = Console.ReadLine();
+string b = Console.ReadLine();
+
+int numA = int.Parse(a);
+int numB = int.Parse(b);
+
+Console.WriteLine("Среднее арифметическое");
+Console.WriteLine((numA + numB) / 2);
+Console.WriteLine("Среднее геометрическое");
+Console.WriteLine(Math.Sqrt(numA * numB));
---
+++ ConsoleApp1/third.cs
@@ -0,0 +1,9 @@
+﻿string a = Console.ReadLine();
+string b  = Console.ReadLine();
+int numA = int.Parse(a);
+int numB = int.Parse(b);
+
+double gipo = Math.Sqrt(Math.Pow(numA, 2) + Math.Pow(numB,2));
+int s = (numA * numB) / 2;
+Console.WriteLine("Гипотенуза" + gipo);
+Console.WriteLine("Площадь" +  s);
