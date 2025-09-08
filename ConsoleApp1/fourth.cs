742d416e3a1c3fead3a6de47c988663dac69e95c
Gnom204 <gnomek100500@gmail.com>
2025-09-08 19:44:54 +0300

    featured: теперь точно В С Ё

--- ConsoleApp1/fourth.cs
+++ ConsoleApp1/fourth.cs
@@ -1,5 +1,12 @@
 ﻿//е) a^2 , a^5 , a^17 за шесть операций.
-
 //a^2 == a*1*1*1*1*1*a
 //a^5 == a*a*a*a*a*1
-//a^17 == 
+//a^17 ==:
+string a = Console.ReadLine();
+double a = int.Parse(a);
+
+double a2 = a * a;
+double a4 = a2 * a2;
+double a8 = a4 * a4;
+double a16 = a8 * a8;
+double a17 = a16 * a;
