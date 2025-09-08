64ef816870e27f53ee1d263ac7db0ecf0e5b0a46
Gnom204 <gnomek100500@gmail.com>
2025-09-08 18:59:38 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿string input = Console.ReadLine();
+
+int numberOfCube = int.Parse(input);
+
+double v = Math.Pow(numberOfCube, 3);
+double s = 4*Math.Pow(numberOfCube, 2);
+
+Console.WriteLine("Объем куба: " + v);
+Console.WriteLine("Площадь боковой поверхности: " + s);
